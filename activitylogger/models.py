51f437dc075b72d354a2623529d4177275032bd5
Jimmy Aguilera <jimmyaguilera010699@gmail.com>
2025-01-17 21:01:00 -0300

    feat(activitylogger): make activity field ManyToMany in TimeLogger
    - Refactored the `activity` field in the `TimeLogger` model to be a `ManyToManyField`.
    - Allows associating multiple activities to a single time log entry.
    - Updated the `Activity` model to ensure proper relationships.

--- activitylogger/models.py
+++ activitylogger/models.py
@@ -3,8 +3,12 @@ from django.contrib.auth.models import User
 from django.utils.timezone import localtime
 
 # Create your models here.
+class Activity(models.Model):
+    name = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 class TimeLogger(models.Model):
-    activity = models.CharField(max_length=200)
+    activities = models.ManyToManyField(Activity, blank=True)
     time_start = models.DateTimeField(auto_now_add=True)
     time_end = models.DateTimeField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
